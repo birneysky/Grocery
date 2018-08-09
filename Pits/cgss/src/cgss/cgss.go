9f3f7225aa7380b75b3e4b47696a1cd4de67f384
yingwei0831 <birneysky@163.com>
2018-08-10 00:59:32 +0800

    m

--- Pits/cgss/src/cgss/cgss.go
+++ Pits/cgss/src/cgss/cgss.go
@@ -43,12 +43,47 @@ func wsHandler(w http.ResponseWriter, r* http.Request) {
 		conn.Close()
 }
 
+
+type Interger int
+
+func (a Interger) Less(b Interger) bool {
+	return a < b
+}
+
+func (a Interger) Add(b Interger) Interger {
+	return  a + b
+}
+
+func (a *Interger)Sub(b Interger)  {
+	  *a -= b
+}
+
+
+
+type UserInfo struct {
+	ID string
+	Name string
+	Address string
+}
+
 func main() {
 	//time.Sleep(10*time.Second)
 	fmt.Println("Hello World 你好， 世界!  are you ok ")
-	//Add(1,2)
-	//http.ListenAndServe(addr: "0.0.0.0:8080",handler: nil)
-	//http.ListenAndServe(addr:"xxxxx",handler:nil)
+	var a Interger = 5
+	if a.Less(2) {
+		a.Add(2)
+	} else {
+		a.Sub(4)
+	}
+	fmt.Print("a = ",a)
+
+	//// key为string value为 UserInfo
+	var infos map[string] UserInfo
+	infos = make(map[string] UserInfo)
+	infos["123455"] = UserInfo{"12345","Tom cat","romm 302"}
+	infos["1"] = UserInfo{"1","toney","shen zheng shi "}
+	
+
 	http.HandleFunc("/ws",wsHandler)
 
 	error := http.ListenAndServe(":8080",nil)
