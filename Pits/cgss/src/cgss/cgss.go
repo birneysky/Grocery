b3ae392faed4d3cf46ab22dc3248792d7d88520a
yingwei0831 <birneysky@163.com>
2018-08-11 01:00:04 +0800

    m

--- Pits/cgss/src/cgss/cgss.go
+++ Pits/cgss/src/cgss/cgss.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+
+	"two"
+	"one"
 )
 
 func Add(first int, second int) int {
@@ -11,10 +13,10 @@ func Add(first int, second int) int {
 
 
 
-func wsHandler(w http.ResponseWriter, r* http.Request) {
-	w.Write([]byte("hello"))
-
-}
+//func wsHandler(w http.ResponseWriter, r* http.Request) {
+//	w.Write([]byte("hello"))
+//
+//}
 
 
 type Interger int
@@ -39,6 +41,48 @@ type UserInfo struct {
 	Address string
 }
 
+
+
+type File struct {
+
+}
+
+func (f *File) Read(buf []byte) (n int, err error) {
+	return 0,nil
+}
+
+func (f* File) Write(buf []byte) (n int, err error) {
+	return 0, nil
+}
+
+func (f* File) Seek(off int64, whence int) (pos int64, err error) {
+	return 0,nil
+}
+
+func (f* File) Close() error {
+	return nil
+}
+
+type IFile interface {
+	Read(buf []byte) (n int, err error)
+	Write(buf []byte) (n int, err error)
+	Seek(off int64,whence int) (pos int64, err error)
+	Close() error
+}
+
+type IReader interface {
+	Read(buf []byte) (n int, err error)
+}
+
+type IWriter interface{
+	Write(buf []byte) (n int, err error)
+}
+
+type ICloser interface {
+	Close() error
+}
+
+
 func main() {
 	//time.Sleep(10*time.Second)
 	fmt.Println("Hello World 你好， 世界!  are you ok ")
@@ -48,7 +92,7 @@ func main() {
 	} else {
 		a.Sub(4)
 	}
-	fmt.Print("a = ",a)
+	fmt.Println("a = ",a)
 
 	//// key为string value为 UserInfo
 	var infos map[string] UserInfo
@@ -56,10 +100,55 @@ func main() {
 	infos["123455"] = UserInfo{"12345","Tom cat","romm 302"}
 	infos["1"] = UserInfo{"1","toney","shen zheng shi "}
 
+	person, ok := infos["1234"]
+	if ok {
+		fmt.Println("Found person",person.Name,"with ID 1234")
+	} else {
+		fmt.Println("Did not find person with ID 1234.")
+	}
 
-	http.HandleFunc("/ws",wsHandler)
 
-	error := http.ListenAndServe(":8080",nil)
-	fmt.Print(error)
+	var (
+		file1 IFile
+		file2 IReader
+		file3 IWriter
+		file4 ICloser
+	)
+
+	file1 = new(File)
+	file2 = new(File)
+	file3 = new(File)
+	file4 = new(File)
+
+	file1.Close()
+	file2.Read([]byte("123"))
+	file3.Write([]byte("456"))
+	file4.Close()
+
+	/*
+
+	在Go 语言中，这个两个接口(IStream,ReadWriter)实际上并无区别 ,因为
+	1.任何实现了one.ReadWriter接口的类，均实现了two.IStream
+	2.任何one.ReadWriter接口对象可以赋值给two.IStream  反之也是可以的
+	3.在任何地方使用one.ReadWriter接口与使用two.IStream并无差异
+	所以以下代码可以编译过
+	*/
+
+	var (
+		file5 two.IStream
+		file6 one.ReadWriter
+	)
+
+	file5 = new(File)
+	file5.Write([]byte("80"))
+	file6 = file1
+
+	file5 = file6
+
+
+	//http.HandleFunc("/ws",wsHandler)
+
+	//error := http.ListenAndServe(":8080",nil)
+	//fmt.Print(error)
 
 }
---
+++ Pits/cgss/src/one/ReadWriter.go
@@ -0,0 +1,7 @@
+package one
+
+
+type ReadWriter interface {
+	Read(buf []byte) (n int, err error)
+	Write(buf []byte) (n int, err error)
+}
